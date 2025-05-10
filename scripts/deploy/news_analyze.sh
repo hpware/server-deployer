9668c324bc0cd9401bf16000f35fd589724e03b8
元皓yh <hw@yuanhau.com>
2025-05-10 11:07:13 +0800

    Update news_analyze.sh

--- scripts/deploy/news_analyze.sh
+++ scripts/deploy/news_analyze.sh
@@ -4,4 +4,7 @@ cd /deploy/applications/news-analyze
 echo "Building..."
 docker compose build
 echo "Deploying..."
-docker compose up -d
+docker-compose up -d --scale app=2
+sleep 4
+echo "Killing old version..."
+docker-compose up -d --scale app=1
